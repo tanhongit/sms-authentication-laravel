1d8abed1c492c9015d1565d3e2b89fb4263b5d8a
Tan Nguyen <tannp27@gmail.com>
2022-08-30 17:40:13 +0700

    update send sms function

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Twilio\Exceptions\ConfigurationException;
+use Twilio\Exceptions\TwilioException;
 use Twilio\Rest\Client;
 
 class User extends Authenticatable {
@@ -52,22 +54,45 @@ class User extends Authenticatable {
     public function generateCode() {
         $code = rand(1000, 9999);
 
-        UserCode::updateOrCreate(['user_id' => auth()->user()->id], ['code' => $code]);
+        UserCode::updateOrCreate(
+            ['user_id' => auth()->user()->id],
+            ['code' => $code]
+        );
 
         $receiverNumber = auth()->user()->phone;
         $message = "2FA login code is " . $code;
 
         try {
-
-            $account_sid = getenv("TWILIO_SID");
-            $auth_token = getenv("TWILIO_TOKEN");
-            $twilio_number = getenv("TWILIO_FROM");
-
-            $client = new Client($account_sid, $auth_token);
-            $client->messages->create($receiverNumber, ['from' => $twilio_number, 'body' => $message]);
-
+            self::sendSms($receiverNumber, $message);
         } catch (Exception $e) {
             $this->info("Error: " . $e->getMessage());
         }
+    }
+
+    /**
+     * @throws ConfigurationException
+     * @throws TwilioException
+     */
+    public static function sendSms(string $tel, string $text)
+    {;
+        $list_tel_vn = [
+            getenv("PHONE_TO_NUMBER_1"),
+            getenv("PHONE_TO_NUMBER_2"),
+        ];
+        if (in_array($tel, $list_tel_vn)) {
+            $to = "+84" . substr($tel, 1, strlen($tel));
+        } else {
+            $to = "+81" . substr($tel, 1, strlen($tel));
+        }
+
+        $account_sid = getenv("TWILIO_SID");
+        $auth_token = getenv("TWILIO_TOKEN");
+        $twilio_number = getenv("TWILIO_FROM");
+
+        $client = new Client($account_sid, $auth_token);
+        $message = $client->messages->create(
+            $to,
+            ['from' => $twilio_number, 'body' =>  $text]
+        );
     }
 }
